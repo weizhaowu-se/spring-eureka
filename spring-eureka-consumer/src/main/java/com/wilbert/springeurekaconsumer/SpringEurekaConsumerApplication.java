69a0a04dd307a400af392ac04274920824330af4
weizhaowu@outlook.com <weizhaowu@outlook.com>
2020-04-09 22:05:19 +0800

    init consumer

---
+++ spring-eureka-consumer/src/main/java/com/wilbert/springeurekaconsumer/SpringEurekaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.wilbert.springeurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaConsumerApplication.class, args);
+	}
+
+}
---
+++ spring-eureka-consumer/src/test/java/com/wilbert/springeurekaconsumer/SpringEurekaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilbert.springeurekaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
