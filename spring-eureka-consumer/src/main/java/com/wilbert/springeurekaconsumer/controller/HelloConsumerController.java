56abfaf0541ae67ccadb4c3839126332deffba28
weizhaowu@outlook.com <weizhaowu@outlook.com>
2020-04-28 22:41:32 +0800

    修改服务注册中心为nacos

--- spring-eureka-consumer/src/main/java/com/wilbert/springeurekaconsumer/controller/HelloConsumerController.java
+++ spring-eureka-consumer/src/main/java/com/wilbert/springeurekaconsumer/controller/HelloConsumerController.java
@@ -1,11 +1,17 @@
 package com.wilbert.springeurekaconsumer.controller;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.wilbert.springeurekaconsumer.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * @author wuweizhao
@@ -19,13 +25,22 @@ public class HelloConsumerController {
 	@Autowired
 	HelloService helloService;
 
-	@RequestMapping(value = "/helloConsumer", method = RequestMethod.GET)
+	@RequestMapping(value = "/helloConsumer", method = GET)
 	public String helloConsumer() {
 		return restTemplate.getForEntity("http://SERVICE-HELLO/hello", String.class).getBody();
 	}
 
-	@RequestMapping(value = "/helloConsumerFeignClient", method = RequestMethod.GET)
+	@RequestMapping(value = "/helloConsumerFeignClient", method = GET)
 	public String helloConsumerFeignClient() {
 		return helloService.hello();
+	}
+
+	@NacosInjected
+	private NamingService namingService;
+
+	@RequestMapping(value = "/get", method = GET)
+	@ResponseBody
+	public List<Instance> get(@RequestParam String serviceName) throws NacosException {
+		return namingService.getAllInstances(serviceName);
 	}
 }
